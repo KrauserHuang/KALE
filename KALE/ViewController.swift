5c7f9e2fbe3caaf32f20f2dce2f9f6fa24db14a7
KrauserHuang <crescentdence@gmail.com>
2022-12-01 14:23:02 +0800

    Initial Commit

---
+++ KALE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KALE
+//
+//  Created by Tai Chin Huang on 2022/12/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
